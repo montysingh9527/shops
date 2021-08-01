82b297c3bff770067e0748e22d28a9e477194735
montysingh <montysingh9527@gmail.com>
2021-08-01 16:39:02 +0800

    设置请求拦截器

--- shop/src/main.js
+++ shop/src/main.js
@@ -10,6 +10,12 @@ import './assets/css/global.css'
 import axios from 'axios'
 // 配置请求的跟路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 设置请求拦截器,因为有些api需要权限才能访问,请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
+axios.interceptors.request.use(config=>{
+  //为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 // 将axios挂载到Vue实例上，通过this.$http访问
 Vue.prototype.$http = axios
 
