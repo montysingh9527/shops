0dc4221ca5d490d17fe6de7e0fb21bed9422fe8a
montysingh <montysingh9527@gmail.com>
2021-08-04 01:26:51 +0800

    用户列表添加用户弹窗ui效果实现

--- shop/src/components/users/User.vue
+++ shop/src/components/users/User.vue
@@ -16,9 +16,31 @@
                     </el-input>
                 </el-col>
                 <el-col :span="6">
-                    <el-button type="primary">添加用户</el-button>
+                    <el-button type="primary" @click="addDialogVisible = true">添加用户</el-button>
                 </el-col>
-            </el-row>
+                <!-- 添加用户对话框 -->
+                <el-dialog title="添加用户"  :visible.sync="addDialogVisible"  width="40%" >
+                    <el-form ref="addFormRef" :model="addForm" :rules="addFormRules" label-width="70px">
+                        <!-- 用户名 -->
+                        <el-form-item label="用户名" prop="username">
+                            <el-input v-model="addForm.username" prefix-icon="el-icon-user" placeholder="请输入用户名"></el-input>
+                        </el-form-item>  
+                        <el-form-item label="密码" prop="password">
+                            <el-input v-model="addForm.password" prefix-icon="el-icon-view" placeholder="请输入密码"></el-input>
+                        </el-form-item>
+                        <el-form-item label="邮箱" prop="email">
+                            <el-input v-model="addForm.email" prefix-icon="el-icon-chat-line-square" placeholder="请输入Email邮箱"></el-input>
+                        </el-form-item>
+                        <el-form-item label="手机号" prop="mobile">
+                            <el-input v-model="addForm.ipone" prefix-icon="el-icon-mobile-phone" placeholder="请输入手机号"></el-input>
+                        </el-form-item>      
+                    </el-form>
+                    <span slot="footer" class="dialog-footer">
+                        <el-button @click="addDialogVisible = false">取 消</el-button>
+                        <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+                    </span>
+                </el-dialog>  
+            </el-row>            
             <!-- 用户列表区域 -->
             <el-table :data="userlist" border stripe>
                 <el-table-column type="index"></el-table-column>
@@ -50,7 +72,7 @@
             :current-page="queryInfo.pagenum" :page-sizes="[3, 5, 10, 20]" :page-size="queryInfo.pagesize" 
             layout="total, sizes, prev, pager, next, jumper" :total="total">
             </el-pagination>
-        </el-card>        
+        </el-card>              
     </div>
 </template>
 
@@ -70,6 +92,35 @@ export default {
             userlist: [],
             // 管理员数据总条数
             total: 0,
+            // 控制显示隐藏添加用户对话框
+            addDialogVisible:false,
+            // 添加用户的表单数据
+            addForm:{
+                    username:'',
+                    password:'',
+                    email:'',
+                    mobile:''
+            },
+            // 添加用户表单的验证规则
+            addFormRules:{
+                 // 验证用户名是否合法
+                username: [
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
+                    { min: 5, max: 10, message: '长度在 5 到 10 个字符', trigger: 'blur' }
+                ],
+                password: [
+                    { required: true, message: '请输入密码', trigger: 'blur' },
+                    { min: 5, max: 10, message: '长度在 6 到 10 个字符', trigger: 'blur' }
+                ],
+                email: [
+                    { required: false, message: '请输入邮箱', trigger: 'blur' },
+                    { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+                ],
+                mobile: [
+                    { required: false, message: '请输入手机号', trigger: 'blur' },
+                    { min: 9, max: 15, message: '长度在 9 到 15 个字符', trigger: 'blur' }
+                ],
+            }
         }
     },
     created(){
