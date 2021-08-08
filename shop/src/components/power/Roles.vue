41a0375fb4d69492a52972860bdc20d90615ce4e
montysingh <montysingh9527@gmail.com>
2021-08-08 23:02:02 +0800

    角色列表-删除角色权限完成

--- shop/src/components/power/Roles.vue
+++ shop/src/components/power/Roles.vue
@@ -21,7 +21,7 @@
         <el-button type="primary" @click="postRolesInfo">确 定</el-button>
     </span>
     </el-dialog>
-    <!-- 编辑角色权限 -->
+    <!-- 编辑角色权限弹窗 -->
     <el-dialog title="修改角色" :visible.sync="editdialogVisibleRoles" width="30%">
     <el-form :model="editRolesForm" label-width="100px">
             <el-form-item label="角色名称：">
@@ -40,6 +40,31 @@
     <el-card class="box-card">
         <el-button type="primary" @click="dialogVisibleRoles = true">添加角色</el-button>
         <el-table :data="rolesList" border stripe>
+            <el-table-column type="expand">
+                <template slot-scope="scope">
+                <!-- <pre>{{scope.row}}</pre>  -->
+                    <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '', 'vcenter']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
+                        <!-- 渲染一级权限 -->
+                        <el-col :span="5">
+                            <el-tag closable @close="removeRightById(scope.row, item1.id)">{{item1.authName}}</el-tag>
+                            <i class="el-icon-caret-right"></i>
+                        </el-col>
+                        <!-- 渲染二级和三级权限 -->
+                        <el-col :span="19">
+                            <!-- 通过 for 循环 嵌套渲染二级权限 -->
+                            <el-row :class="[i2 === 0 ? '' : 'bdtop', 'vcenter']" v-for="(item2, i2) in item1.children" :key="item2.id">
+                                <el-col :span="6">
+                                    <el-tag type="success" closable @close="removeRightById(scope.row, item2.id)">{{item2.authName}}</el-tag>
+                                    <i class="el-icon-caret-right"></i>
+                                </el-col>
+                                <el-col :span="18">
+                                    <el-tag type="warning" v-for="item3 in item2.children" :key="item3.id" closable @close="removeRightById(scope.row, item3.id)">{{item3.authName}}</el-tag>
+                                </el-col>
+                            </el-row>
+                        </el-col>
+                    </el-row>
+                </template>
+            </el-table-column>
             <el-table-column type="index"></el-table-column>
             <el-table-column label="角色名称" prop="roleName"></el-table-column>
             <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
@@ -146,11 +171,45 @@ export default {
           })         
         }).catch((err => err)
         )
+    },
+    // 根据Id删除对应的权限
+    async removeRightById(role, rightId) {
+        // 弹框提示用户是否要删除
+        const result = await this.$confirm(
+            '是否删除该权限？',
+            '提示',
+            {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+            }
+        ).catch(err => err)
+        if (result !== 'confirm') {
+            return this.$message.info('取消了删除！')
+        }
+        // 请求路径：roles/:roleId/rights/:rightId  - 请求方法：delete
+        const { data: res } = await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+        if (res.meta.status !== 200) {
+            return this.$message.error('删除权限失败！')
+        }
+        role.children = res.data
     }
 }     
 }
 </script>
 
 <style lang="less" scoped>
-
+.el-tag {
+  margin: 7px;
+}
+.bdtop {
+  border-top: 1px solid #eee;
+}
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
+.vcenter {
+  display: flex;
+  align-items: center;
+}
 </style>
