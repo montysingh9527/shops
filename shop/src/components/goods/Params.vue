264a2f871b8104785e44da41235be5b91d813673
montysingh <montysingh9527@gmail.com>
2021-10-02 14:25:50 +0800

    商品管理-分类参数-选择商品分类

--- shop/src/components/goods/Params.vue
+++ shop/src/components/goods/Params.vue
@@ -12,7 +12,10 @@
        <el-alert title="注意：只允许为第三级分类设置相关参数！" type="warning" :closable="false" show-icon></el-alert>
        <!-- 选择商品分类 -->
        <el-row class="cat_opt">
-           <el-col>选择商品分类：</el-col>
+           <el-col>选择商品分类：
+               <!-- 选择商品分类级联选择器 -->
+               <el-cascader expand-trigger="hover" v-model="cateKeys" :options="cateList" :props="cateProps"  @change="handleChange"></el-cascader>
+           </el-col>       
        </el-row>
     </el-card>
 </div>
@@ -21,14 +24,36 @@
     export default {
         data() {
             return {
-
+                // 商品分类列表
+                cateList: [],
+                // 级联选择框的配置对象
+                cateProps: {
+                    value: 'cat_id',
+                    label: 'cat_name',
+                    children: 'children'
+                },
+                // 级联选择框双向绑定到的数组
+                cateKeys: []
             }
         },
         created() {
-
+            this.getCateList()
         },
         methods: {
-
+            getCateList(){
+                this.$http.get('/categories').then(res=>{
+                   this.cateList = res.data.data
+                   console.log('cat',this.cateList)
+                })
+            },
+            handleChange(){
+                // 证明选中的不是三级分类
+                console.log(this.cateKeys)
+                if (this.cateKeys.length !== 3) {
+                    this.cateKeys = []
+                    return
+                }
+            }
         }
     }
 </script>
